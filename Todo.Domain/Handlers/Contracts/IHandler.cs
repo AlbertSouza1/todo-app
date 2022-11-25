21ec704a0b5fd2d1bf78e355f63a79c919eb6d37
Albert Souza <albert.souza@inovafarma.com.br>
2022-11-25 20:41:28 -0300

    cria interface de handlers

---
+++ Todo.Domain/Handlers/Contracts/IHandler.cs
@@ -0,0 +1,10 @@
+﻿using Todo.Domain.Commands.Inputs.Contracts;
+using Todo.Domain.Commands.Results.Contracts;
+
+namespace Todo.Domain.Handlers.Contracts
+{
+    public interface IHandler<T> where T : ICommand
+    {
+        ICommandResult Handle(T command);
+    }
+}
