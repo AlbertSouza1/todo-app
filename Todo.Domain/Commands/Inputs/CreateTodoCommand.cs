31252727c3726cb7e7c6e8a537f1ee7a3b1d6d96
Albert Souza <albert.souza@inovafarma.com.br>
2022-11-25 20:10:41 -0300

    cria command de marcar tarefa como concluida

--- Todo.Domain/Commands/Inputs/CreateTodoCommand.cs
+++ Todo.Domain/Commands/Inputs/CreateTodoCommand.cs
@@ -7,6 +7,17 @@ namespace Todo.Domain.Commands.Inputs
 {
     public class CreateTodoCommand : Notifiable<Notification>, ICommand
     {
+        public CreateTodoCommand()
+        {
+        }
+
+        public CreateTodoCommand(string title, string user, DateTime date)
+        {
+            Title = title;
+            User = user;
+            Date = date;
+        }
+
         public string Title { get; set; }
         public string User { get; set; }
         public DateTime Date { get; set; }
