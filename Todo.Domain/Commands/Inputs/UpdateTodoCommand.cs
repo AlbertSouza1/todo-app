d1fdb06cf066dae8ad9fb05d1e1cfcd800297985
Albert Souza <albert.souza@inovafarma.com.br>
2022-11-25 20:29:19 -0300

    cria command de alteracao de tarefa

---
+++ Todo.Domain/Commands/Inputs/UpdateTodoCommand.cs
@@ -0,0 +1,37 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+using Todo.Domain.Commands.Inputs.Contracts;
+
+namespace Todo.Domain.Commands.Inputs
+{
+    public class UpdateTodoCommand : Notifiable<Notification>, ICommand
+    {
+        public UpdateTodoCommand()
+        {
+        }
+
+        public UpdateTodoCommand(Guid id, string title, string user)
+        {
+            Id = id;
+            Title = title;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string User { get; set; }
+
+        public bool Validate()
+        {
+            AddNotifications(
+                new Contract<Notification>()
+                .Requires()
+                .IsNotNullOrEmpty(Title, "Title", "Preencha o título da tarefa.")
+                .IsNotNullOrEmpty(User, "User", "Usuário inválido.")
+                );
+
+            return IsValid;
+        }
+    }
+}
