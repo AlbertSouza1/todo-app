df6ecfe8f6a44f780ffcbe1108b39db9a0009216
Albert Souza <albert.souza@inovafarma.com.br>
2022-11-26 19:30:58 -0300

    cria testes para o handler de criacao de tarefa

--- Todo.Domain/Handlers/TodoHandler.cs
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -6,11 +6,12 @@ using Todo.Domain.Commands.Results;
 using Todo.Domain.Commands.Results.Contracts;
 using Todo.Domain.Handlers.Contracts;
 using Todo.Domain.Repositories;
+using Todo.Domain.Shareds.Validations.Utils;
 
 namespace Todo.Domain.Handlers
 {
     public class TodoHandler : 
-        Notifiable<Notification>,
+        Validation,
         IHandler<CreateTodoCommand>,
         IHandler<UpdateTodoCommand>
     {
@@ -24,13 +25,13 @@ namespace Todo.Domain.Handlers
         public ICommandResult Handle(CreateTodoCommand command)
         {
             if (!command.Validate())
-                return new CommandResult(false, command.Messages);
+                return new CommandResult(success:false, command.Messages);
 
             var todo = command.GetTodoItem();
 
             _todoRepository.Create(todo);
 
-            return new CommandResult(success: true, "Tarefa salva com sucesso.", todo);
+            return new CommandResult(success:true, "Tarefa salva com sucesso.", todo);
         }
 
         public ICommandResult Handle(UpdateTodoCommand command)
---
+++ Todo.Tests/DomainTests/Handlers/TodoHandlerTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using Todo.Domain.Commands.Inputs;
+using Todo.Domain.Commands.Results;
+using Todo.Domain.Handlers;
+using Todo.Domain.Repositories;
+
+namespace Todo.Tests.DomainTests.Handlers
+{
+    [TestClass]
+    public class TodoHandlerTest
+    {
+        private readonly TodoHandler _handler;
+        private readonly Mock<ITodoRepository> _repository;
+
+        public TodoHandlerTest()
+        {
+            _repository = new Mock<ITodoRepository>();
+            _handler = new TodoHandler(_repository.Object);
+        }
+
+        [TestMethod]
+        public void Given_an_invalid_todoCommand_should_return_fail_result()
+        {
+            //Arrange
+            var command = new CreateTodoCommand("", "Usuario", DateTime.Now);           
+
+            //Act
+            var result = (CommandResult)_handler.Handle(command);
+
+            //Assert
+            Assert.IsFalse(result.Success);
+        }
+
+        [TestMethod]
+        public void Given_a_valid_todoCommand_should_create_todo()
+        {
+            //Arrange
+            var command = new CreateTodoCommand("Titulo", "Usuario", DateTime.Now);
+
+            //Act
+            var result = (CommandResult)_handler.Handle(command);
+
+            //Assert
+            Assert.IsTrue(result.Success);
+        }
+    }
+}
