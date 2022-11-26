d9803fc938f75f652a83dde77c8313c206e88bd2
Albert Souza <albert.souza@inovafarma.com.br>
2022-11-26 15:58:02 -0300

    cria testes de unidade para classe de validacao

---
+++ Todo.Tests/DomainTests/Shareds/Validations/Utils/Mocks/FakeValidation.cs
@@ -0,0 +1,28 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using Todo.Domain.Shareds.Validations.Utils;
+
+namespace Todo.Tests.DomainTests.Shareds.Validations.Utils.Mocks
+{
+    internal class FakeValidation : Validation
+    {
+        public FakeValidation(int id, string name, string address)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+
+            AddNotifications(
+                new Contract<Notification>()
+                .Requires()
+                .IsGreaterThan(Id, 0, "Id", "Id inválido.")
+                .IsNotNullOrEmpty(Name, "Name", "Nome vazio.")
+                .IsNotNullOrEmpty(Name, "Name", "Endereço vazio.")
+                );
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Todo.Tests/DomainTests/Shareds/Validations/Utils/ValidationTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Tests.DomainTests.Shareds.Validations.Utils.Mocks;
+
+namespace Todo.Tests.DomainTests.Shareds.Validations.Utils
+{
+    [TestClass]
+    public class ValidationTest
+    {
+        [TestMethod]
+        public void Messages_should_return_all_messages_separeted_by_line_break()
+        {
+            //Arrange & Act
+            var sut = new FakeValidation(0, "", "");
+
+            //Assert
+            Assert.AreEqual("Id inválido.\r\nNome vazio.\r\nEndereço vazio.", sut.Messages);
+        }
+
+        [TestMethod]
+        public void Messages_should_return_empty_string_if_it_has_no_notifications()
+        {
+            //Arrange & Act
+            var sut = new FakeValidation(1, "Teste", "Teste");
+
+            //Assert
+            Assert.AreEqual(string.Empty, sut.Messages);
+        }
+
+        [TestMethod]
+        public void FirstMessage_should_return_first_notification_message()
+        {
+            //Arrange & Act
+            var sut = new FakeValidation(0, "", "");
+
+            //Assert
+            Assert.AreEqual("Id inválido.", sut.FirstMessage);
+        }
+
+        [TestMethod]
+        public void FirstMessage_should_return_empty_string_if_it_has_no_notifications()
+        {
+            //Arrange & Act
+            var sut = new FakeValidation(1, "Teste", "Teste");
+
+            //Assert
+            Assert.AreEqual(string.Empty, sut.FirstMessage);
+        }
+    }
+}
