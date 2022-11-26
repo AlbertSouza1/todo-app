f05ddc6484b042dd3d3aef581a41b25b9bb3a140
Albert Souza <albert.souza@inovafarma.com.br>
2022-11-25 21:18:23 -0300

    cria handler

---
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,32 @@
+﻿using Flunt.Notifications;
+using System;
+using Todo.Domain.Commands.Inputs;
+using Todo.Domain.Commands.Results.Contracts;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Handlers
+{
+    public class TodoHandler : 
+        Notifiable<Notification>,
+        IHandler<CreateTodoCommand>,
+        IHandler<UpdateTodoCommand>
+    {
+        private readonly ITodoRepository _todoRepository;
+
+        public TodoHandler(ITodoRepository todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICommandResult Handle(UpdateTodoCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
