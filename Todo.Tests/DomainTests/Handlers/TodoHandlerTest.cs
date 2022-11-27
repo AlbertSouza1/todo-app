2c982e2011d40a222b31797e2c2af872172cd76c
Albert Souza <albert.souza@inovafarma.com.br>
2022-11-27 20:25:17 -0300

    cria testes de unidade para handle de alteracao de estado de conclusao de tarefa

--- Todo.Tests/DomainTests/Handlers/TodoHandlerTest.cs
+++ Todo.Tests/DomainTests/Handlers/TodoHandlerTest.cs
@@ -67,7 +67,6 @@ namespace Todo.Tests.DomainTests.Handlers
         public void Given_a_valid_updateTodoCommand_should_update_todo_title()
         {
             //Arrange
-            var todoItem = new TodoItem("Titulo", DateTime.Now, "Usuário");
             var command = new UpdateTodoCommand(Guid.NewGuid(), "Novo Titulo", "Usuario");
 
             _repository.Setup(x => x.GetById(It.IsAny<Guid>(), It.IsAny<string>())).Returns(_validTodoItem);
@@ -79,6 +78,55 @@ namespace Todo.Tests.DomainTests.Handlers
             //Assert
             Assert.IsTrue(result.Success);
             Assert.AreEqual("Novo Titulo", todoUpdated.Title);
+        }
+
+        [TestMethod]
+        public void Given_an_invalid_markTodoCommand_should_return_fail_result()
+        {
+            //Arrange
+            var command = new MarkTodoCommand(id: Guid.NewGuid(), user: string.Empty, done: true);
+            _repository.Setup(x => x.GetById(It.IsAny<Guid>(), It.IsAny<string>())).Returns(_validTodoItem);
+
+            //Act
+            var result = (CommandResult)_handler.Handle(command);
+
+            //Assert
+            Assert.IsFalse(result.Success);
+        }
+
+        [TestMethod]
+        public void If_markTodoCommand_marks_state_to_done_should_update_todoItem_state_to_done()
+        {
+            //Arrange
+            var command = new MarkTodoCommand(id: Guid.NewGuid(), user: "Usuario", done: true);
+
+            _repository.Setup(x => x.GetById(It.IsAny<Guid>(), It.IsAny<string>())).Returns(_validTodoItem);
+
+            //Act
+            var result = (CommandResult)_handler.Handle(command);
+            var todoUpdated = (TodoItem)result.Data;
+
+            //Assert
+            Assert.IsTrue(result.Success);
+            Assert.IsTrue(todoUpdated.Done);
+        }
+
+        [TestMethod]
+        public void If_markTodoCommand_marks_state_to_undone_should_update_todoItem_state_to_undone()
+        {
+            //Arrange
+            var command = new MarkTodoCommand(id: Guid.NewGuid(), user: "Usuario", done: false);
+            _validTodoItem.ChangeMarkState(done: true);
+
+            _repository.Setup(x => x.GetById(It.IsAny<Guid>(), It.IsAny<string>())).Returns(_validTodoItem);
+
+            //Act
+            var result = (CommandResult)_handler.Handle(command);
+            var todoUpdated = (TodoItem)result.Data;
+
+            //Assert
+            Assert.IsTrue(result.Success);
+            Assert.IsFalse(todoUpdated.Done);
         }
     }
 }
