15ba14c7b8e7cb5ab29472ee86cab813da2fc86e
Albert Souza <albert.souza@inovafarma.com.br>
2022-11-27 15:41:26 -0300

    cria testes de unidade para handle de atualizacao de tarefa

--- Todo.Tests/DomainTests/Handlers/TodoHandlerTest.cs
+++ Todo.Tests/DomainTests/Handlers/TodoHandlerTest.cs
@@ -3,6 +3,7 @@ using Moq;
 using System;
 using Todo.Domain.Commands.Inputs;
 using Todo.Domain.Commands.Results;
+using Todo.Domain.Entities;
 using Todo.Domain.Handlers;
 using Todo.Domain.Repositories;
 
@@ -13,15 +14,17 @@ namespace Todo.Tests.DomainTests.Handlers
     {
         private readonly TodoHandler _handler;
         private readonly Mock<ITodoRepository> _repository;
+        private readonly TodoItem _validTodoItem;
 
         public TodoHandlerTest()
         {
             _repository = new Mock<ITodoRepository>();
             _handler = new TodoHandler(_repository.Object);
+            _validTodoItem = new TodoItem("Titulo", DateTime.Now, "Usuário");
         }
 
         [TestMethod]
-        public void Given_an_invalid_todoCommand_should_return_fail_result()
+        public void Given_an_invalid_createTodoCommand_should_return_fail_result()
         {
             //Arrange
             var command = new CreateTodoCommand("", "Usuario", DateTime.Now);           
@@ -34,7 +37,7 @@ namespace Todo.Tests.DomainTests.Handlers
         }
 
         [TestMethod]
-        public void Given_a_valid_todoCommand_should_create_todo()
+        public void Given_a_valid_createTodoCommand_should_create_todo()
         {
             //Arrange
             var command = new CreateTodoCommand("Titulo", "Usuario", DateTime.Now);
@@ -44,6 +47,38 @@ namespace Todo.Tests.DomainTests.Handlers
 
             //Assert
             Assert.IsTrue(result.Success);
+        }
+
+        [TestMethod]
+        public void Given_an_invalid_updateTodoCommand_should_return_fail_result()
+        {
+            //Arrange
+            var command = new UpdateTodoCommand(Guid.NewGuid(), title:"", "Usuario");
+            _repository.Setup(x => x.GetById(It.IsAny<Guid>(), It.IsAny<string>())).Returns(_validTodoItem);
+
+            //Act
+            var result = (CommandResult)_handler.Handle(command);
+
+            //Assert
+            Assert.IsFalse(result.Success);
+        }
+
+        [TestMethod]
+        public void Given_a_valid_updateTodoCommand_should_update_todo_title()
+        {
+            //Arrange
+            var todoItem = new TodoItem("Titulo", DateTime.Now, "Usuário");
+            var command = new UpdateTodoCommand(Guid.NewGuid(), "Novo Titulo", "Usuario");
+
+            _repository.Setup(x => x.GetById(It.IsAny<Guid>(), It.IsAny<string>())).Returns(_validTodoItem);
+
+            //Act
+            var result = (CommandResult)_handler.Handle(command);
+            var todoUpdated = (TodoItem)result.Data;
+
+            //Assert
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual("Novo Titulo", todoUpdated.Title);
         }
     }
 }
