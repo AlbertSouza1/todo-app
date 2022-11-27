252c1ccd36300526087d2f8bdddc9be1600a51a7
Albert Souza <albert.souza@inovafarma.com.br>
2022-11-27 15:12:52 -0300

    cria testes de unidade para todoitem

---
+++ Todo.Tests/DomainTests/Entities/TodoItemTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Domain.Entities;
+
+namespace Todo.Tests.DomainTests.Entities
+{
+    [TestClass]
+    public class TodoItemTest
+    {
+        [TestMethod]
+        public void Given_a_new_todoItem_done_should_be_false()
+        {
+            //Arrange & Act
+            var sut = new TodoItem("Titulo", DateTime.Now, "Usuário");
+
+            //Assert
+            Assert.IsFalse(sut.Done);
+        }
+    }
+}
