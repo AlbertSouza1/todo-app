6fc69c9bfb298d199809353702a5c4cd338ca03f
Albert Souza <albert.souza@inovafarma.com.br>
2022-12-03 10:06:04 -0300

    especifica tipo das colunas no mapeamento do banco de tarefas

---
+++ Todo.Infra/Contexts/TodoDataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Entities;
+
+namespace Todo.Infra.Contexts
+{
+    public class TodoDataContext : DbContext
+    {
+        public TodoDataContext(DbContextOptions<TodoDataContext> options) : base(options) { }
+
+        public DbSet<TodoItem> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().Property(x => x.Id);
+            modelBuilder.Entity<TodoItem>().Property(x => x.User).HasMaxLength(120).HasColumnType("varchar(120)");
+            modelBuilder.Entity<TodoItem>().Property(x => x.Title).HasMaxLength(120).HasColumnType("varchar(160)");
+            modelBuilder.Entity<TodoItem>().Property(x => x.Done).HasColumnType("bit");
+            modelBuilder.Entity<TodoItem>().Property(x => x.Date);
+            modelBuilder.Entity<TodoItem>().HasIndex(b => b.User);
+        }
+    }
+}
