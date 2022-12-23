1e767d100bc01edb0d29396b8ff9655a0eadc255
Albert Souza <albert.souza@inovafarma.com.br>
2022-12-23 17:52:26 -0300

    cria get all controller

--- Todo.Api/Controllers/TodoController.cs
+++ Todo.Api/Controllers/TodoController.cs
@@ -1,18 +1,26 @@
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
 using Todo.Domain.Commands.Inputs;
-using Todo.Domain.Commands.Results;
 using Todo.Domain.Commands.Results.Contracts;
+using Todo.Domain.Entities;
 using Todo.Domain.Handlers;
+using Todo.Domain.Repositories;
 
 namespace Todo.Api.Controllers
 {
+    [ApiController]
+    [Route("v1/todos")]
     public class TodoController : ControllerBase
     {
         [Route("")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll([FromServices] ITodoRepository repository)
+            => repository.GetAll("albert");
+
+        [Route("")]
         [HttpPost]
-        public ICommandResult Create([FromBody] CreateTodoCommand command, [FromServices] TodoHandler handler)
+        public ICommandResult Create([FromBody] CreateTodoCommand command,
+                                     [FromServices] TodoHandler handler)
         {
             command.User = "albert";
             return handler.Handle(command);
