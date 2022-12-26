378b6d2af66e9cdcab37d573cadf0af524fe9180
Albert Souza <albert.souza@inovafarma.com.br>
2022-12-26 14:06:41 -0300

    restringe acesso nao autorizado controller

--- Todo.Api/Controllers/TodoController.cs
+++ Todo.Api/Controllers/TodoController.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Todo.Domain.Commands.Inputs;
 using Todo.Domain.Commands.Results.Contracts;
 using Todo.Domain.Entities;
@@ -11,49 +13,71 @@ namespace Todo.Api.Controllers
 {
     [ApiController]
     [Route("v1/todos")]
+    [Authorize]
     public class TodoController : ControllerBase
     {
         [Route("")]
         [HttpGet]
         public IEnumerable<TodoItem> GetAll([FromServices] ITodoRepository repository)
-            => repository.GetAll(user: "albert");
+        {
+            var user = GetUser();
+            return repository.GetAll(user);
+        }
 
         [Route("done")]
         [HttpGet]
         public IEnumerable<TodoItem> GetAllDone([FromServices] ITodoRepository repository)
-            => repository.GetAllDone(user: "albert");
+        {
+            var user = GetUser();
+            return repository.GetAllDone(user);
+        }
 
         [Route("undone")]
         [HttpGet]
         public IEnumerable<TodoItem> GetAllUndone([FromServices] ITodoRepository repository)
-            => repository.GetAllUndone(user: "albert");
+        {
+            var user = GetUser();
+            return repository.GetAllUndone(user);
+        }
 
         [Route("done/today")]
         [HttpGet]
         public IEnumerable<TodoItem> GetDoneForToday([FromServices] ITodoRepository repository)
-            => repository.GetByPeriod(user: "albert", date: DateTime.Now.Date, done: true);
+        {
+            var user = GetUser();
+            return repository.GetByPeriod(user, date: DateTime.Now.Date, done: true);
+        }
 
         [Route("undone/today")]
         [HttpGet]
         public IEnumerable<TodoItem> GetUndoneForToday([FromServices] ITodoRepository repository)
-            => repository.GetByPeriod(user: "albert", date: DateTime.Now.Date, done: false);
+        {
+            var user = GetUser();
+            return repository.GetByPeriod(user, date: DateTime.Now.Date, done: false);
+        }
 
         [Route("done/tomorrow")]
         [HttpGet]
         public IEnumerable<TodoItem> GetDoneForTomorrow([FromServices] ITodoRepository repository)
-            => repository.GetByPeriod(user: "albert", date: DateTime.Now.Date.AddDays(1), done: true);
+        {
+            var user = GetUser();
+            return repository.GetByPeriod(user, date: DateTime.Now.Date.AddDays(1), done: true);
+        }
 
         [Route("undone/tomorrow")]
         [HttpGet]
         public IEnumerable<TodoItem> GetUndoneForTomorrow([FromServices] ITodoRepository repository)
-            => repository.GetByPeriod(user: "albert", date: DateTime.Now.Date.AddDays(1), done: false);
+        {
+            var user = GetUser();
+            return repository.GetByPeriod(user, date: DateTime.Now.Date.AddDays(1), done: false);
+        }
 
         [Route("")]
         [HttpPost]
         public ICommandResult Create([FromBody] CreateTodoCommand command,
                                      [FromServices] TodoHandler handler)
         {
-            command.User = "albert";
+            command.User = GetUser();
             return handler.Handle(command);
         }
 
@@ -62,7 +86,7 @@ namespace Todo.Api.Controllers
         public ICommandResult Update([FromBody] UpdateTodoCommand command,
                                      [FromServices] TodoHandler handler)
         {
-            command.User = "albert";
+            command.User = GetUser();
             return handler.Handle(command);
         }
 
@@ -71,8 +95,10 @@ namespace Todo.Api.Controllers
         public ICommandResult ChangeState([FromBody] MarkTodoCommand command,
                                      [FromServices] TodoHandler handler)
         {
-            command.User = "albert";
+            command.User = GetUser();
             return handler.Handle(command);
         }
+
+        private string GetUser() => User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
     }
 }
